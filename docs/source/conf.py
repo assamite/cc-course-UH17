3e4cd68061366cedab9aefa0d75b3f5545c3e2c8
Simo Linkola <slinkola@cs.helsinki.fi>
2017-09-04 11:50:18 +0300

    Initial changes to docs for new course instance

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,8 +61,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'CC-MAS 2016'
-copyright = '2016, Simo Linkola'
+project = 'CC-Course-UH 2017'
+copyright = '2017, Simo Linkola'
 author = 'Simo Linkola'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -141,7 +141,7 @@ html_theme_options = {
 #html_title = "Computational Creativity and Multi-Agent Systems"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "CC-MAS 2016"
+html_short_title = "CC-Course UH 2017"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -226,7 +226,7 @@ html_sidebars = {
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ComputationalCreativityandMulti-AgentSystemsfall2016doc'
+htmlhelp_basename = 'ComputationalCreativityfall2017doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -248,7 +248,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'ComputationalCreativityandMulti-AgentSystemsfall2016.tex', 'Computational Creativity and Multi-Agent Systems, fall 2016 Documentation',
+  (master_doc, 'ComputationalCreativityfall2017.tex', 'Computational Creativity, fall 2017 Documentation',
    'Simo Linkola', 'manual'),
 ]
 
@@ -278,7 +278,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'computationalcreativityandmulti-agentsystemsfall2016', 'Computational Creativity and Multi-Agent Systems, fall 2016 Documentation',
+    (master_doc, 'computationalcreativityfall2017', 'Computational Creativity, fall 2017 Documentation',
      [author], 1)
 ]
 
@@ -292,8 +292,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'ComputationalCreativityandMulti-AgentSystemsfall2016', 'Computational Creativity and Multi-Agent Systems, fall 2016 Documentation',
-   author, 'ComputationalCreativityandMulti-AgentSystemsfall2016', 'One line description of project.',
+  (master_doc, 'ComputationalCreativityfall2017', 'Computational Creativity, fall 2017 Documentation',
+   author, 'ComputationalCreativityfall2017', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,16 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Computational Creativity and Multi-Agent Systems, fall 2016
+Computational Creativity, University of Helsinki, fall 2017
 =====================================================================================
 
 .. centered::
 	*The documentation is largely in development. Expect constant changes.*
 
-`(repository with the full code examples) <https://github.com/assamite/cc-mas-course>`_
+`(repository with the full code examples) <https://github.com/assamite/cc-course-UH17>`_
 
 This documentation holds tips and sample code relevant to 
-`the course <https://www.cs.helsinki.fi/courses/582759/2016/s/k/1>`_. All the
+`the course <https://courses.helsinki.fi/fi/DATA15002/119122268>`_. All the
 code is written with **Python 3.5** in mind. Although many of the snippets work on
 earlier Python versions, we recommend installing Python 3.5 straight away (see
 :doc:`setup`).
